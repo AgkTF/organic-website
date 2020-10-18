0f7f691cf1434f193e63264f6c6b3cf929b8bdf7
agkz1st <ahmed.agk22@gmail.com>
2020-10-18 09:03:07 +0200

    Initial project structure and necessary files.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        libre: "Libre Baskerville",
+      },
+      colors: {
+        turquoise: "#00DBD0",
+        lturquoise: "#F1FEFE",
+        gblue: "#374B5C",
+      },
+      screens: {
+        "2xl": "1366px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
