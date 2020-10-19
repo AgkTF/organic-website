e398a96ab5fcab7d6553ddaeb118d689890eff57
agkz1st <ahmed.agk22@gmail.com>
2020-10-19 22:52:27 +0200

    fix the bug in postcss config file.

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
     tailwindcss,
     process.env.NODE_ENV === "production" &&
       purgecss({
-        content: ["./src/**/*.js", "./public/index.html"],
+        content: ["./pages/*.html"],
         defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       }),
   ],
